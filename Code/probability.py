69ecf83a7b81d45ae997e2fd8fd43c01830d7104
Samuel Drapeau <samuel.drapeau@gmail.com>
2024-12-22 05:21:13 +0800

    all

---
+++ Code/probability.py
@@ -0,0 +1,13 @@
+#%%
+
+from lib.standard_lib import *
+from IPython.display import display, HTML
+
+
+import plotly
+plotly.offline.init_notebook_mode()
+display(HTML(
+    '<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_SVG"></script>'
+))
+#%%
+
